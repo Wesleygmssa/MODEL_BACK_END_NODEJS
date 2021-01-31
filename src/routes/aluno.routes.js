f5bd4b2fcfc01c3236dd85e3caebfe733fe04f66
wesley guerra <guerra_ssa@hotmail.com>
2021-01-31 09:18:47 -0300

    fechando rotas

--- src/routes/aluno.routes.js
+++ src/routes/aluno.routes.js
@@ -1,13 +1,14 @@
 // class e funcções contrutora inica com letra Maiuscula
 import express from 'express';
 import alunoController from '../controllers/AlunoController';
+import loginRequired from '../middlewares/loginRequired';
 
 const router = express.Router();
 
 router.get('/', alunoController.index);
-router.post('/', alunoController.store);
+router.post('/', loginRequired, alunoController.store);
 router.get('/:id', alunoController.show);
-router.put('/:id', alunoController.update);
-router.delete('/:id', alunoController.delete);
+router.put('/:id', loginRequired, alunoController.update);
+router.delete('/:id', loginRequired, alunoController.delete);
 
 export default router; // app.js
